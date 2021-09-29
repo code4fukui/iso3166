593b4e58af8fab2c1d263c7c32a1f5881699813e
Taisuke Fukuno <fukuno@jig.jp>
2021-09-29 09:50:41 +0900

    update comment

--- make.js
+++ make.js
@@ -5,7 +5,7 @@ const csv = await CSV.fetch("iso3166-1_temp.csv");
 const csv2 = csv.filter(l => l[1].length); // インデックス行を取り除く
 const data = CSV.toJSON(csv2); // CSVからJSONへ
 const notmatch = data.filter(d => !d["各行政区分"].endsWith(d["alpha-2"])); // 各行政区はalpha-2か？
-assert(notmatch.length == 0); // 確認できたので削除
+assert(notmatch.length == 0); // 確認できたので削除しよう
 const data2 = data.map(d => ({ alpha3: d["alpha-3"], alpha2: d["alpha-2"], numeric: d.numeric, name: d["ISO 3166-1における英語名"], name_ja: d["国・地域名"], area: d["場所"] })); // 名前変更
 
 const makeDataJS = (name, json) => `export const ${name} = ${JSON.stringify(json)};\n`;
